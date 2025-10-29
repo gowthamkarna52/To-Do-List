7c6490d17692a86174228f5963d6f84dfbb79a01
GOWTHAM S <160300982+gowthamkarna52@users.noreply.github.com>
2025-10-29 07:09:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do List 📝
+    - Organize your daily tasks efficiently — add, view, and delete items with ease!
+    - Built in Java with a simple menu-driven interface for quick task management.
---
+++ To_Do_List.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class To_Do_List{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String> tasks= new ArrayList<>();
+        int choice;
+
+        do{
+            System.out.println("===To-Do List Menu===");
+            System.out.println("1.View Tasks");
+            System.out.println("2.Add Task");
+            System.out.println("3.Delete Task");
+            System.out.println("4.Exit");
+            System.out.println("Choose an Option (1-4): ");
+            choice =sc.nextInt();
+            sc.nextLine();
+            switch(choice){
+                case 1:
+                    if(tasks.isEmpty()) System.out.println("No Tasks yet.");
+                    else{ 
+                        for(int i=0;i<tasks.size();i++) 
+                        System.out.println((i+1)+"."+tasks.get(i));
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter Task: ");
+                    String task=sc.nextLine();
+                    tasks.add(task);
+                    System.out.println("Task added.");
+                    break;
+                case 3:
+                    if(tasks.isEmpty()) System.out.println("No Tasks to delete.");
+                    else{ 
+                        for(int i=0;i<tasks.size();i++) 
+                        System.out.println((i+1)+"."+tasks.get(i));
+                        System.out.println("Enter task number to delete: ");
+                        int index=sc.nextInt();
+                        sc.nextLine();
+                        if(index>0 && index<=tasks.size()){ 
+                            String removed=tasks.remove(index-1);
+                            System.out.println("Deleted: "+ removed);
+                        }
+                        else{
+                            System.out.println("Invalid task number.");
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option.");                  
+            }
+        }  
+        while(choice!=4);
+        sc.close();
+    }
+}
